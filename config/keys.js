2c237bd0419c02a199f3366932a46f17a5477d2f
DevAbas <abas.memmedov.94@gmail.com>
2019-02-13 23:07:54 +0400

    Created Login router on api to login user

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin123@ds161653.mlab.com:61653/dev-connector'
+  mongoURI: 'mongodb://admin:admin123@ds161653.mlab.com:61653/dev-connector',
+  secretOrKey: 'secret'
 };
