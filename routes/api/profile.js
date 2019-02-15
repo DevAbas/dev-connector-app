0a517af118833c2cbeed511d89b019d0714ff9a3
DevAbas <abas.memmedov.94@gmail.com>
2019-02-15 12:05:39 +0400

    Get profile endpoint added to api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const express  = require('express');
+const router   = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load models
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
 /**
 * @route  GET api/profile/test
@@ -7,5 +13,28 @@ const router = express.Router();
 * @access Public
 **/
 router.get('/test', (req, res) => res.send({ msg: 'Profile Works' }));
+
+/**
+* @route  GET api/profile
+* @desc   Get user's profile
+* @access Private
+**/
+router.get(
+  '/', 
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if(!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors)
+        }
+        return res.json(profile)
+      })
+      .catch(err => res.status(404).json(err))
+  }
+)
 
 module.exports = router;
