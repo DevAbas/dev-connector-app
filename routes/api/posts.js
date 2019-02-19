0624c28f53b815b86488c939a447bcece5183fde
DevAbas <abas.memmedov.94@gmail.com>
2019-02-19 14:27:51 +0400

    Added create post endpoint

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+// Load model
+const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 /**
 * @route  GET api/posts/test
@@ -7,5 +15,31 @@ const router = express.Router();
 * @access Public
 **/
 router.get('/test', (req, res) => res.send({ msg: 'Posts Works' }));
+
+/**
+* @route  POST api/posts
+* @desc   Create post
+* @access Private
+**/
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  // Validation create post inputs
+  const { errors, isValid } = validatePostInput(req.body);
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+
+  // Create new post
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  })
+    
+
+  // Save post
+  newPost.save().then(post => res.json(post));
+})
 
 module.exports = router;
