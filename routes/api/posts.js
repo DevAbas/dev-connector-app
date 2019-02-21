c2dce4e8312f7a4865730db59064c565be0cf8fe
DevAbas <abas.memmedov.94@gmail.com>
2019-02-21 12:25:06 +0400

    Delete and get post by id enpoint added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 // Load model
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // Validation
 const validatePostInput = require('../../validation/post');
@@ -37,9 +38,56 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     user: req.user.id
   })
     
-
   // Save post
   newPost.save().then(post => res.json(post));
+});
+
+/**
+* @route  DELETE api/posts/:id
+* @desc   Delete post
+* @access Private
+**/
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ notauthorized: 'User not authorized' })
+          }
+
+          // Delete post
+          post.remove().then(() => res.json({ success: true }))
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+    })
+  
+});
+
+/**
+* @route  GET api/posts
+* @desc   Get all posts
+* @access Public
+**/
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json(err))
 })
+
+/**
+* @route  GET api/posts/:id
+* @desc   Get a post by id 
+* @access Public
+**/
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopostfound: 'No Post found with that ID' }))
+})
+
 
 module.exports = router;
